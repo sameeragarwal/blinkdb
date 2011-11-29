97f08a432186190fcad3bd21c6eaf2d27ebd6ea4
Sameer Agarwal <sameerag@cs.berkeley.edu>
2011-11-29 15:42:43 -0800

    Table Re-writing Logic/Cost Model in place

--- shark-new/src/main/scala/SharkSemanticAnalyzer.scala
+++ shark-new/src/main/scala/SharkSemanticAnalyzer.scala
@@ -16,6 +16,10 @@ import org.apache.hadoop.hive.ql.plan.PlanUtils
 import org.apache.hadoop.hive.ql.plan.MoveWork
 import org.apache.hadoop.hive.ql.session.SessionState
 
+//@sameerag
+import org.apache.hadoop.hive.ql.costmodel._
+import scala.collection.mutable.ListBuffer
+
 import java.util.ArrayList
 
 import scala.collection.JavaConversions._
@@ -28,6 +32,86 @@ class SharkSemanticAnalyzer(conf: HiveConf) extends SemanticAnalyzer(conf){
 
   var moveTasks = new ArrayList[MoveTask]()
 
+  /*
+  @SuppressWarnings("nls")
+  private void WalkAST(ASTNode ast, List<ASTNode> tableNameList, Bounds bounds) {
+	  if (ast != null) {
+		  for (int i=0; i<ast.getChildCount(); i++){
+			  ASTNode child = (ASTNode) ast.getChild(i);
+			  switch (child.getToken().getType()) {
+			  case HiveParser.TOK_TABNAME:
+				  	ASTNode tableNameNode = (ASTNode) child.getChild(0);
+				  	tableNameList.add(tableNameNode);
+				  break;
+			  case HiveParser.TOK_WITHMAXERROR:
+			    ASTNode errorBoundNode = (ASTNode) child.getChild(0);
+			    bounds.errorBound = Double.parseDouble(errorBoundNode.token.getText());
+			    break;
+			  case HiveParser.TOK_INTIME:
+			    ASTNode timeBoundNode = (ASTNode) child.getChild(0);	  
+			    bounds.timeBound = Double.parseDouble(timeBoundNode.token.getText());
+			    break;
+			  default:
+				  break;
+			  }
+			  WalkAST(child, tableNameList, bounds);
+		  }
+	  }
+  }
+  */
+
+/**
+* Walk the AST, and get all the table name nodes in a list. 
+*/
+
+def WalkAST(ast: ASTNode, tableNameList: ListBuffer[ASTNode], bounds: Bounds): Unit = {
+  if (ast != null) {
+    for (i <- 0 to ast.getChildCount() -1){
+      var child: ASTNode = ast.getChild(i).asInstanceOf[ASTNode];
+      child.getToken().getType() match {
+      	case HiveParser.TOK_TABNAME =>
+      		var tableNameNode: ASTNode = child.getChild(0).asInstanceOf[ASTNode];
+      		tableNameList.add(tableNameNode);
+      	case HiveParser.TOK_WITHMAXERROR =>
+      		var errorBoundNode: ASTNode = child.getChild(0).asInstanceOf[ASTNode];
+      		bounds.errorBound = java.lang.Double.parseDouble(errorBoundNode.token.getText());
+      	case HiveParser.TOK_INTIME =>
+      		var timeBoundNode: ASTNode = child.getChild(0).asInstanceOf[ASTNode];	  
+      		bounds.timeBound = java.lang.Double.parseDouble(timeBoundNode.token.getText());
+      	case _ => 
+	  }
+      
+      WalkAST(child, tableNameList, bounds);
+    }
+  }  
+}
+  
+    /**
+   * @name replaceTableNameWithSampleName
+   * @author api, sameerag
+   * @description Replace the full table name with the sample table name in the AST.
+   */
+def replaceTableNameWithSampleName(ast: ASTNode) = {
+
+  //var tableNameList: List[ASTNode]  = new ArrayList[ASTNode]();
+  var tableNameList = new ListBuffer[ASTNode];
+  var timeErrorBound: Bounds  = new Bounds();
+	  
+  WalkAST(ast, tableNameList, timeErrorBound);
+  if (timeErrorBound.isInitialized()) {
+    var costModel: CostModel = CostModel.getInstance();
+	var iterator: Iterator[ASTNode]  = tableNameList.iterator();
+	while (iterator.hasNext()) {
+	  var tableNameNode: ASTNode = iterator.next();
+	  var sampledTableName: String = costModel.getSampledTableName(timeErrorBound.errorBound, timeErrorBound.timeBound);
+	  if (sampledTableName != null) {
+	    LOG.info("Replacing table [" + tableNameNode.token.getText() + "] with [" + sampledTableName + "]");
+	    tableNameNode.token.setText(sampledTableName);
+	  }
+	}
+  }
+}
+  
   override def analyzeInternal(ast: ASTNode): Unit = {
     reset()
 
@@ -39,6 +123,9 @@ class SharkSemanticAnalyzer(conf: HiveConf) extends SemanticAnalyzer(conf){
     var child: ASTNode = ast;
 
     LOG.info("Starting Semantic Analysis");
+    
+    //@sameerag: Adding Anand's table-rewriting logic
+    replaceTableNameWithSampleName(ast);
 
     // analyze create table command
     if (ast.getToken().getType() == HiveParser.TOK_CREATETABLE) {

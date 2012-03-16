5f2092053b2267a06d5a1c8ad44987deb644e9ad
Sameer Agarwal <sameerag@cs.berkeley.edu>
2012-03-16 04:13:41 -0700

    Added Conviva UDFs to Bootstrap. Also marks Sachin's 100th 100 :-)

--- bootstrap/bootstrap.py
+++ bootstrap/bootstrap.py
@@ -7,6 +7,42 @@ import commands
 def mean(X):
     return sum(X)/ float(len(X))
  
+def percentile_99(X):
+  return stat.scoreatpercentile(X,99)
+
+def percentile_95(X):
+  return stat.scoreatpercentile(X,95)
+
+def median(X):
+  return stat.scoreatpercentile(X,50)
+
+def non_zero_avg(X):
+  count = 0
+  nzsum = 0
+  for i in X:
+    if i != 0:
+      count += 1
+      nzsum += i
+  return float(nzsum)/count
+
+def top_k_avg(X):
+  # K=100
+  Y = sorted(X)
+  size = len(Y)
+  top_Y = []
+  for i in range(1,100):
+    top_Y.append(Y[size-i])
+  return mean(top_Y)
+
+def even_number_avg(X):
+  count = 0
+  esum = 0
+  for i in X:
+    if i%2 == 0:
+      count += 1
+      esum += i
+  return float(esum)/count
+
 def bootstrap(sample, samplesize = None, nsamples = 300, statfunc = mean):
     """
     Arguments:
@@ -30,7 +66,8 @@ def bootstrap(sample, samplesize = None, nsamples = 300, statfunc = mean):
         X.append(x)
     return X
 
-def main():
+def bootstrap_wrapper(data, func, fname):
+  """
   # Read data from file and store it in 'data'
   #data = [1,2,3,4,5,6,7,8,9,10]
   data = []
@@ -42,8 +79,9 @@ def main():
   f = open("session_time.txt")
   for line in f:
     data.append(float(line))
+  """
 
-  true_answer = mean(data)
+  true_answer = func(data)
   print "True Answer", true_answer
 
   error = []
@@ -51,8 +89,8 @@ def main():
   f_error = open('error.txt', 'w')
   f_bars = open('error_bars.txt', 'w')
 
-  for size in xrange(1000, 11000, 1000):
-    ans = bootstrap(data, samplesize=size)
+  for size in xrange(1000, 33000, 1000):
+    ans = bootstrap(data, samplesize=size, statfunc=func)
     avg = average(ans)
     sd = std(ans)
     print avg, sd
@@ -70,10 +108,10 @@ def main():
   gf_error_w = open("error.plt", "w")
 
   c = gf_error.read()
-  c = c.replace("_gnuplot_output_", "error.pdf")
+  c = c.replace("_gnuplot_output_", "error_" + fname + ".pdf")
   c = c.replace("_gnuplot_xlabel_", "Sample Size (MB)")
   c = c.replace("_gnuplot_ylabel_", "% Error")
-  c = c.replace("_gnuplot_title_", "Mean")
+  c = c.replace("_gnuplot_title_", fname)
   gf_error_w.write(c)
   gf_error_w.close()
 
@@ -83,16 +121,43 @@ def main():
   gf_bars_w = open("error_bars.plt", "w")
 
   c = gf_bars.read()
-  c = c.replace("_gnuplot_output_", "error_bars.pdf")
+  c = c.replace("_gnuplot_output_", "error_bars_" + fname + ".pdf")
   c = c.replace("_gnuplot_xlabel_", "Sample Size (MB)")
   c = c.replace("_gnuplot_ylabel_", "Statistical Answer \\n(with error bars)")
   c = c.replace("_gnuplot_true_answer_", str(true_answer))
-  c = c.replace("_gnuplot_title_", "Mean")
+  c = c.replace("_gnuplot_title_", fname)
 
   gf_bars_w.write(c)
   gf_bars_w.close()
  
   commands.getoutput("gnuplot error_bars.plt")
-  
+
+def main():
+  # Read data from file and store it in 'data'
+  #data = [1,2,3,4,5,6,7,8,9,10]
+  data = []
+
+  # Gaussian
+  #for i in xrange(0,100000):
+  #  data.append(gauss(5, 1))
+
+  f = open("session_time.txt")
+  for line in f:
+    data.append(float(line))
+
+  fn = {}
+  fn[mean] = "Mean"
+  fn[std] = "Standard Deviation"
+  fn[var] = "Variance"
+  fn[percentile_99] = "99th Percentile"
+  fn[percentile_95] = "75th Percentile"
+  fn[non_zero_avg] = "Non Zero Average"
+  fn[top_k_avg] = "Top K Average"
+  fn[even_number_avg] = "Average of Even Numbers"
+  fn[median] = "Median"
+
+  for k,v in fn.iteritems():
+    bootstrap_wrapper(data, k, v)
+
 if __name__ == "__main__":
   main()

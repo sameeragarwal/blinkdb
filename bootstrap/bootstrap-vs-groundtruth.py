783f97411d577255b0d03f3f60ea44f2c9d3f023
Sameer Agarwal <sameerag@cs.berkeley.edu>
2012-03-23 10:12:48 -0700

    Code cleanup for review

--- bootstrap/bootstrap-vs-groundtruth.py
+++ bootstrap/bootstrap-vs-groundtruth.py
@@ -94,22 +94,6 @@ def bootstrap(sample, samplesize = None, nsamples = 300, statfunc = mean):
     return X
 
 def bootstrap_wrapper(data, func, fname, cf=None):
-  """
-  # Read data from file and store it in 'data'
-  #data = [1,2,3,4,5,6,7,8,9,10]
-  data = []
-
-  # Gaussian
-  #for i in xrange(0,100000):
-  #  data.append(gauss(5, 1))
-
-  f = open("session_time.txt")
-  for line in f:
-    data.append(float(line))
-  """
-
-  #random.shuffle(data)
-
   f = open(fname+'relative_error.txt', 'w')
   f2 = open(fname+'varying_k.txt', 'w')
   true_answer = func(data)
@@ -146,57 +130,6 @@ def bootstrap_wrapper(data, func, fname, cf=None):
     print "Ground Truth", true_answer, sd
     f.write('\t' + str(float(sd)/true_answer)+'\n')
     f2.write('\n')
-  
-  f.close()
-  error = []
-  x = []
-  f_error = open('error.txt', 'w')
-  f_bars = open('error_bars.txt', 'w')
-
-  """ 
-  for size in xrange(1000, 11000, 1000):
-    ans = bootstrap(data, samplesize=size, statfunc=func)
-    avg = average(ans)
-    sd = std(ans)
-    print avg, sd
-    error.append(sd)
-    x.append(size/1000)
-    f_error.write(str(2*size/1000)+'\t'+str((100*1.96*sd/avg))+'\n')
-    f_bars.write(str(2*size/1000)+'\t'+str(avg)+'\t'+str(avg-1.96*sd)+'\t'+str(avg+1.96*sd)+'\n')
-  """
-
-  f_error.close()
-  f_bars.close()
-
-  #Make GNUPLOT graphs
-
-  gf_error = open("error.template.plt")
-  gf_error_w = open("error.plt", "w")
-
-  c = gf_error.read()
-  c = c.replace("_gnuplot_output_", "error_" + fname + ".pdf")
-  c = c.replace("_gnuplot_xlabel_", "Sample Size (MB)")
-  c = c.replace("_gnuplot_ylabel_", "% Error")
-  c = c.replace("_gnuplot_title_", fname)
-  gf_error_w.write(c)
-  gf_error_w.close()
-
-  commands.getoutput("gnuplot error.plt")
-
-  gf_bars = open("error_bars.template.plt")
-  gf_bars_w = open("error_bars.plt", "w")
-
-  c = gf_bars.read()
-  c = c.replace("_gnuplot_output_", "error_bars_" + fname + ".pdf")
-  c = c.replace("_gnuplot_xlabel_", "Sample Size (MB)")
-  c = c.replace("_gnuplot_ylabel_", "Statistical Answer \\n(with error bars)")
-  c = c.replace("_gnuplot_true_answer_", str(true_answer))
-  c = c.replace("_gnuplot_title_", fname)
-
-  gf_bars_w.write(c)
-  gf_bars_w.close()
- 
-  commands.getoutput("gnuplot error_bars.plt")
 
 def main():
   # Read data from file and store it in 'data'
@@ -207,7 +140,7 @@ def main():
   #for i in xrange(0,100000):
   #  data.append(gauss(5, 1))
 
-  f = open("session_time_20gb.txt")
+  f = open("session_time_50gb.txt")
   #f = open("session_time.txt")
   for line in f:
     data.append(float(line))
